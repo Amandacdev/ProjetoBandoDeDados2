b07a8c7f060ea65e2b547c599fb3c929393fd972
Amanda Cruz de Araújo <amanda.crz.aj@gmail.com>
2023-06-09 16:03:33 -0300

    Update Outros.sql

--- arquivos/Outros.sql
+++ arquivos/Outros.sql
@@ -33,6 +33,56 @@ call monitorarAtividades('georgelima');
 
 -- Functions:
 
+--• 1 função que use SUM, MAX, MIN, AVG ou COUNT
+--Função que retorna o número de comentários de um determinado post
+Create or replace function somaComentarios(post varchar)
+Returns integer
+As $$
+Declare qtde_comentarios varchar(25);
+Begin	
+	SELECT COUNT(c.url_coment) into qtde_comentarios
+	FROM postagem p
+	JOIN comentario c ON p.url = c.url_post
+	WHERE p.url = post
+	GROUP BY p.url, p.titulo;
+return qtde_comentarios;
+End; $$ LANGUAGE 'plpgsql';
+
+select somaComentarios('www.domain.com/post/ianribeiro/3');
+
+--Função que retorna a quantidade de postagens feitas em determinado período
+Create or replace function post_por_data(data1 date, data2 date)
+Returns table(
+		url2 varchar,
+		autor2 varchar,
+		titulo2 varchar,
+		conteudo2 text,
+		datahora2 timestamp) As $$
+BEGIN
+	return query
+		select *
+		from postagem 
+		where datahora between data1 and data2;
+End; $$ 
+LANGUAGE 'plpgsql';
+
+select post_por_data('2011-10-08 ','2023-10-16');
+
+--Função que retorna o número de postagens de usuários de uma determinada idade (informação importante para estudo de usuários)
+Create or replace function post_por_idade(idade integer)
+Returns integer
+As $$
+Declare qtde_posts varchar(25);
+Begin
+	select count(u.email) into qtde_posts
+	from usuario u
+	inner join postagem p
+	on p.autor = u.email
+	where (select round((current_date - datanasc)/365)) = idade;
+return qtde_posts;
+End; $$ LANGUAGE 'plpgsql';
+
+select post_por_idade(32);
 
 -- Triggers:
 
